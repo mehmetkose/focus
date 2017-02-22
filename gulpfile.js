91b67ddb9d4e5a4628af9adaed90e71246d6f9af
Mehmet Kose <mehmetkose122@gmail.com>
2017-02-22 14:26:14 +0300

    add scaffold tast

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,12 @@ const errorify = require('errorify');
 const gutil = require('gulp-util');
 const source = require('vinyl-source-stream');
 const browserify = require('browserify');
-var fs = require('fs');
+
+const checkdir = require('checkdir');
+const mkdirp = require('mkdirp');
+const exec = require('child_process').exec;
+
+var settings = require('./package.json');
 
 const instanceBrowserify = browserify({
                               entries: ['./src/app.jsx'],
@@ -54,6 +59,7 @@ server = () =>
             log: 'debug'
         }))
 
+
 move = (event) =>
     gulp.src(event.path, { base: 'src' })
         .pipe(gulp.dest('dist'))
@@ -62,11 +68,39 @@ moveAll = () =>
     gulp.src(filesWatch, { base: 'src'} )
         .pipe(gulp.dest('dist'))
 
+scaffold = () => {
+    let appSlug = settings.app.appSlug || "hello";
+    let appName = settings.app.appName || "HelloWorld";
+    let companySlug = settings.app.companySlug || "focus";
+
+    checkdir('tmp/', { ignoreDotFiles: true }).then(info => {
+      if (info.empty) {
+        mkdirp('tmp/', function (err) {
+            if (err) console.error(err)
+            else console.log('>>> tmp folder created')
+        });
+      }
+    })
+
+    let commands = [
+      `cd tmp`,
+      `cordova create cordova com.${companySlug}.${appName} ${appName}`,
+    ]
+
+    exec(commands.join(' && '), (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      console.log(err);
+    });
+
+  }
+
 gulp
     .task('bundle', bundler)
     .task('move-static', moveAll)
     .task('watch', watcher)
     .task('clean', cleaner)
     .task('server', server)
+    .task('scaffold', scaffold)
     .task('build', ['bundle', 'move-static'])
     .task('default', ['watch', 'server']);
--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,70 @@ const { h, Component, render, options } = preact;
 import injectTapEventPlugin from 'preact-tap-event-plugin';
 injectTapEventPlugin();
 
-class App extends Component {
+function coords(e) {
+	let t = e.touches && e.touches[0] || e;
+	return { x: t.pageX, y: t.pageY };
+}
+
+const EVENTS = [['mousemove','mouseup'], ['touchmove','touchend']]
+
+class Dragable extends Component {
+	state = {
+		x: 0,
+		y: 0
+	};
+	start = e => {
+		let [move,end] = this.evt = EVENTS[e.type==='touchstart'?1:0];
+		addEventListener(move, this.move);
+		addEventListener(end, this.end);
+	};
+	move = e => {
+		let c = coords(e);
+		if (!this.down) {
+			this.down = c;
+			return;
+		}
+		this.offset = {
+			x: c.x - this.down.x,
+			y: c.y - this.down.y
+		};
+    this.base.style.transform = `translate(${this.offset.x}px, ${this.offset.y}px)`;
+  };
+  end = e => {
+		let [move,end] = this.evt;
+		removeEventListener(move, this.move);
+		removeEventListener(end, this.end);
+		if (this.offset) {
+			this.setState({
+				x: this.state.x + this.offset.x,
+				y: this.state.y + this.offset.y
+			});
+		}
+		this.down = this.offset = null;
+	};
+	// this is just for the demo: shows when we have re-rendered:
+	componentDidUpdate() {
+		this.base.setAttribute('updated', true);
+		setTimeout( () => this.base.removeAttribute('updated'), 150);
+	}
+	render({ }, { x, y }) {
+  	return (
+    	<div class="foo" style={{
+				left: x,
+				top: y,
+				transform: ''
+			}} onMouseDown={this.start} onTouchStart={this.start}>
+      	Drag Me
+				<pre>({x}, {y})</pre>
+      </div>
+    );
+  }
+}
+
+
+
+
+class Clickable extends Component {
 	state = {
 		count: 0
 	};
@@ -23,6 +86,18 @@ class App extends Component {
 				<span>{count}</span>
 			</div>
 		);
+	}
+}
+
+class App extends Component {
+	render() {
+		return(
+			<div>
+				<Clickable/>
+				<br />
+				<Dragable/>
+			</div>
+		)
 	}
 }
 
